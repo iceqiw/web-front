a843077db9f6a5273a7271c3a4eaa1e67beb1d41
iceqiw <qqwei1123@163.com>
2018-05-04 14:08:46 +0800

    modify

---
+++ datasite/src/a_container/home/container/analysis/chartA.js
@@ -0,0 +1,81 @@
+export const charDataA=(datasource={}) =>{
+    console.log(datasource)                   
+    const option={
+      title: { text: '房价' },
+      tooltip: { trigger: 'axis'},
+      xAxis: {
+        ype : 'category',
+        position: 'bottom',
+        boundaryGap: true,
+        axisLine : {    // 轴线
+            show: true,
+            lineStyle: {
+                type: 'solid',
+                width: 1
+            }
+        },
+        data:datasource.map((a)=>a.name)
+
+      },
+      calculable : true,
+      legend: {
+        data: ['总价','面积','单价','租金']
+      },
+      yAxis: [
+        {
+          type : 'value',
+          name : '总价',
+          axisLabel : {
+              formatter: '{value} 万'
+          }
+        },
+        {
+          type : 'value',
+          name : '面积',
+          show: false,
+          axisLabel : {
+              formatter: '{value} 平米'
+          }
+        },
+        {
+          type : 'value',
+          name : '单价',
+          axisLabel : {
+              formatter: '{value} 平米/元'
+          }
+        },
+        {
+          type : 'value',
+          name : '租金',
+          show: false,
+          axisLabel : {
+              formatter: '{value} 元'
+          }
+        }], 
+        series: [{
+          name: '总价',
+          type: 'bar',
+          yAxisIndex: 0,   
+          data: datasource.map((a)=>a.price_total)
+        },{
+          name: '面积',
+          type: 'line',
+          yAxisIndex: 1,
+          smooth: true,//折线图是趋缓的
+          data: datasource.map((a)=>a.acreage)
+        },{
+          name: '单价',
+          type: 'line',
+          yAxisIndex: 2,
+          smooth: true,//折线图是趋缓的
+          data: datasource.map((a)=>a.price)
+        },{
+          name: '租金',
+          type: 'line',
+          yAxisIndex: 3,
+          smooth: true,//折线图是趋缓的
+          data: datasource.map((a)=>a.rent)
+      }]
+    };
+    return option
+  };
--- datasite/src/a_container/home/container/analysis/index.js
+++ datasite/src/a_container/home/container/analysis/index.js
@@ -17,8 +17,9 @@ import 'echarts/lib/component/legend';
 // ==================
 // 组件
 // ==================
-import { actHouseData} from "../../../../a_action/analysis-action";
-
+import {actHouseData} from "../../../../a_action/analysis-action";
+import {charDataA} from "./chartA";
+import {charDataB} from "./chartB";
 
 @connect(
   state => ({ houseinfo: state.analysis.houseinfo}),
@@ -45,121 +46,30 @@ export default class Analysis extends React.Component {
   }
 
   showChart(){
-    let myChart = echarts.init(this.charts);
+    const myChartA = echarts.init(this.Acharts);
     this.props.actions.actHouseData().then(res => {
-      let opt=this.charData(res);
-      myChart.setOption(opt);
+      const opt=charDataA(res);
+      myChartA.setOption(opt);
     });
+
+    // const myChartB = echarts.init(this.Bcharts);
+    // this.props.actions.actHouseData().then(res => {
+    //   const opt=charDataB(res);
+    //   myChartB.setOption(opt);
+    // });
   }
 
-  charData(datasource) {
-    console.log(datasource)
-                      
-    let option={
-      title: { text: '房价' },
-      tooltip: { trigger: 'axis'},
-      xAxis: {
-        ype : 'category',
-        position: 'bottom',
-        boundaryGap: true,
-        axisLine : {    // 轴线
-            show: true,
-            lineStyle: {
-                type: 'solid',
-                width: 1
-            }
-        },
-        data:datasource.map((a)=>a.name)
-
-      },
-      calculable : true,
-      legend: {
-        data: ['总价','面积','单价','租金']
-      },
-      yAxis: [
-        {
-          type : 'value',
-          name : '总价',
-          axisLabel : {
-              formatter: '{value} 万'
-          }
-        },
-        {
-          type : 'value',
-          name : '面积',
-          axisLabel : {
-              formatter: '{value} 平米'
-          }
-        },
-        {
-          type : 'value',
-          name : '单价',
-          axisLabel : {
-              formatter: '{value} 平米/元'
-          }
-        },
-        {
-          type : 'value',
-          name : '租金',
-          show: false,
-          axisLabel : {
-              formatter: '{value} 元'
-          }
-        }], 
-        series: [{
-          name: '总价',
-          type: 'bar',
-          yAxisIndex: 0,
-          itemStyle: {
-            normal: {
-                color: 'rgb(255, 70, 131)'
-              }
-          },
-          data: datasource.map((a)=>a.price_total)
-        },{
-          name: '面积',
-          type: 'bar',
-          barWidth: '30%',
-          itemStyle: {
-            normal: {
-                color: 'rgb(155, 170, 131)'
-              }
-          },
-          data: datasource.map((a)=>a.acreage)
-        },{
-          name: '单价',
-          type: 'line',
-          yAxisIndex: 2,
-          lineStyle: {
-            normal: {
-                width: 2,  //连线粗细
-                color: "#278BDD"  //连线颜色
-              }
-          },
-          smooth: true,//折线图是趋缓的
-          data: datasource.map((a)=>a.price)
-        },{
-          name: '租金',
-          type: 'line',
-          yAxisIndex: 3,
-          lineStyle: {
-            normal: {
-                width: 3,  //连线粗细
-                color: "#A52A2A"  //连线颜色
-              }
-          },
-          smooth: true,//折线图是趋缓的
-          data: datasource.map((a)=>a.rent)
-      }]
-    };
-    return option
-  }
 
   render() {
     return <div> 
        <Row type="flex" align="middle" justify="center">
             <Col xs={24} md={24}>
-            <div ref={(c) => { this.charts = c; }} style={{ width: '100%' , height: '600px' }}></div>
+              <div ref={(c) => { this.Acharts = c; }} style={{ width: '100%' , height: '600px' }}></div>
+            </Col>
+        </Row>
+        <Row type="flex" align="middle" justify="center">
+            <Col xs={24} md={24}>
+              <div ref={(c) => { this.Bcharts = c; }} style={{ width: '100%' , height: '600px' }}></div>
             </Col>
         </Row>
     </div>;
